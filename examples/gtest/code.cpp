7d0afbfbbd3f5fd9503f576fee4e137d338b228d
Dimon <dimon@nomail>
2024-07-10 13:00:42 +0300

    gtest changes

--- examples/gtest/code.cpp
+++ examples/gtest/code.cpp
@@ -1,16 +1,67 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int solve_function(int a, int b)
+#define ll long long
+#define all(a) a.begin(), a.end()
+#define REP(x) for(int i = 0 ; i< x; i ++)
+template <typename T>
+istream &operator>>(istream &in, vector<T> &a)
 {
-  return a + b;
+  for (auto &i : a)
+    in >> i;
+  return in;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  for (auto &i : a)
+  {
+    out << i;
+  }
+  return out;
+}
+
+struct trio{
+  ll ans;ll back;ll summ;
+};
+
+bool dd(ll&A,ll&B){return A>B;}
+
+void solve(ll v, vector<ll> &arr)
+{
+  ll len=0;
+  ll minimum=1e16;
+  for(int i = 0 ; i < 9 ; i ++){
+    len=max(len,v/arr[i]);
+    minimum = min(minimum,arr[i]);
+  }
+  if(len==0){
+    cout<<-1<<'\n';
+  }else{
+    //cout<<len<<'\n';
+    ll last=0;
+    for(int i = 0 ; i < len;i++){
+      for(int o = 8 ; o>= 0 ; o --){
+        if(arr[o] + minimum * (len-i-1)+last <= v){
+          cout<<o+1;
+          last+=arr[o];
+        }
+      }
+    }
+  }
 }
 
 #ifndef GOOGLETEST_INCLUDE_GTEST_GTEST_H_
 
 int main(int argc, char **argv)
 {
-  return solve_function(3, 5);
+  ll v;
+  cin >> v;
+  vector<ll> arr(9);
+  cin >> arr;
+  solve(v,arr);
 }
 
 #endif
--- examples/gtest/tests.cpp
+++ examples/gtest/tests.cpp
@@ -3,20 +3,21 @@
 
 using namespace std;
 
-TEST(TestGroup, Test_1) 
+TEST(TestGroup, Test_1)
 {
-  auto result = solve_function(1,1);
-
-  EXPECT_EQ(2,result);
+  vector<ll> a = {4,1,2,3};
+  vector<ll> b = {7,2,5,6};
+  auto result = solve(4, 2, 3, 2, a,b);
+  EXPECT_EQ("Bodya", result);
 }
 
-TEST(TestGroup, Test_2) 
+TEST(TestGroup, Test_2)
 {
-  auto result = solve_function(5,1);
-
-  EXPECT_NE(2,result);
+  vector<ll> a = {3, 1, 4, 5, 2 ,7 ,8 ,10, 6, 9};
+  vector<ll> b = {5, 10, 5 ,1 ,3 ,7 ,10, 15, 4, 3};
+  auto result = solve(10, 8, 2, 10, a,b);
+  EXPECT_EQ("Sasha", result);
 }
-
 
 int main(int argc, char **argv)
 {
